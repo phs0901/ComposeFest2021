cebd29eb5fef83f414e51face79e74abeded141b
박희성 <huisung.park@hyundai.com>
2021-12-05 01:03:50 +0900

    week1
    week1

---
+++ week 1-Jetpack Compose basics/week1_basic/app/src/main/java/com/phs0901/jetpack/compose/basic/week1/ui/Color.kt
@@ -0,0 +1,8 @@
+package com.phs0901.jetpack.compose.basic.week1.ui
+
+import androidx.compose.ui.graphics.Color
+
+val Navy = Color(0xFF073042)
+val Blue = Color(0xFF4285F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color(0xFFEFF7CF)
---
+++ week 1-Jetpack Compose basics/week1_basic/app/src/main/java/com/phs0901/jetpack/compose/basic/week1/ui/Shape.kt
@@ -0,0 +1,11 @@
+package com.phs0901.jetpack.compose.basic.week1.ui
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(0.dp)
+)
